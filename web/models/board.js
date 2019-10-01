7714525cbc7eeeb99d6489a9472068ab2247837f
hzoou <woojooo52@naver.com>
2019-10-01 18:04:54 +0900

    Add exception handling, when query execution fails
    query 실행 중, 예기치 못한 요청에 대해 query를 실행하는 promise의 reject에 담아 에러 처리
    reject된 값을 model에서 try-catch 문을 통해 response에 상태코드 400을 보냄으로써 유연한 예외 처리

--- web/models/board.js
+++ web/models/board.js
@@ -2,12 +2,15 @@ const board = require('../schema/query');
 
 const BOARD = {
     getBoardIdxByUserId : async (user_id, res) => {
-        const result = (await board.getBoardIdxByUserId(user_id))[0];
-        if (result) return BOARD.getAllListByBoard(result.idx, res);
-        res.status(400).send({ status: 'FAIL', message: '해당 id는 존재하는 board가 없습니다.'});
+        try {
+            const result = (await board.getBoardIdxByUserId(user_id))[0];
+            return BOARD.getAllListByBoard(result.idx, res);
+        } catch (e) {
+            res.status(400).send({ status: 'FAIL', message: '해당 id는 존재하는 board가 없습니다.'});
+        }
     },
 
-    getAllListByBoard: async (boardIdx, res) => {
+    getAllListByBoard : async (boardIdx, res) => {
         res.send({ status: 'SUCCESS', board_idx: boardIdx, data: await board.getAllListByBoard(boardIdx)});
     }
 };
