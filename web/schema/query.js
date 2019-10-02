e43161bffbb58810eac1d89a185c42946292b13b
hzoou <woojooo52@naver.com>
2019-10-02 18:35:21 +0900

    Modify query
    getBoardIdxByUserId : user_id를 검색하여 얻은 idx를 BOARD 테이블의 User_idx와 비교하도록 수정
    getAllListByBoard : getBoardIdxByUserId를 통해 얻은 board_idx와 LIST 테이블의 BOARD_idx를 비교하도록 수정

--- web/schema/query.js
+++ web/schema/query.js
@@ -1,9 +1,9 @@
 const db = require('../database/connection').connect();
 
 module.exports = {
-    getBoardIdxByUserId : async (user_id) => {
+    getBoardIdxByUserId : async (id) => {
         return new Promise((resolve, reject) => {
-            db.execute('SELECT BOARD.idx FROM BOARD, USER WHERE BOARD.idx = (SELECT USER.idx FROM USER WHERE USER.id = ?)', [user_id],function (err, results) {
+            db.execute('SELECT BOARD.idx, BOARD.private FROM BOARD WHERE BOARD.USER_idx = (SELECT USER.idx FROM USER WHERE USER.id = ?)', [id],function (err, results) {
                 if (err) reject(err);
                 resolve(results);
             });
@@ -12,7 +12,7 @@ module.exports = {
 
     getAllListByBoard : async (board_idx) => {
         return new Promise((resolve, reject) => {
-            db.execute('SELECT LIST.idx as LIST_idx, ITEM.idx as ITEM_idx, LIST.title as LIST_title, ITEM.title as ITEM_title FROM BOARD, LIST, ITEM WHERE ITEM.list_idx = LIST.idx AND BOARD.idx = ?', [board_idx], function (err, results) {
+            db.execute('SELECT L.idx as LIST_idx, I.idx as ITEM_idx, L.title as LIST_title, I.title as ITEM_title FROM LIST L JOIN ITEM I ON I.LIST_idx = L.idx AND L.BOARD_idx = ?', [board_idx], function (err, results) {
                 if (err) reject(err);
                 resolve(results);
             })
