7e8abb769253bce342fd57686ff5d91ccab2c9c5
hzoou <woojooo52@naver.com>
2019-10-03 15:06:55 +0900

    Add isAdmin middleware to getUser api
    해당 유저가 관리자라면 사용자 목록을 조회한 결과값을 반환, 관리자가 아니라면 권한이 없으므로 return

--- web/routes/admin.js
+++ web/routes/admin.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const isAdmin = require('../middlewares/isAdmin');
+const USER = require('../models/user');
 
 router.get('/', isAdmin, function(req, res) {
     res.render('admin', { title: 'admin' });
+});
+
+router.get('/user', isAdmin, async function (req, res) {
+    const result = await USER.getAllUser();
+    res.send({ status: 'SUCCESS', data: result });
 });
 
 module.exports = router;
