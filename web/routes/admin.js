126ee31def8e2a5461ae53bf7fcf6e0c7c9e9323
hzoou <woojooo52@naver.com>
2019-10-03 19:50:41 +0900

    Modify admin router
    관리자의 아이디를 보여주기 위해 admin.ejs를 렌더할 때 아이디 값을 넘겨줌

--- web/routes/admin.js
+++ web/routes/admin.js
@@ -4,7 +4,7 @@ const isAdmin = require('../middlewares/isAdmin');
 const USER = require('../models/user');
 
 router.get('/', isAdmin, function(req, res) {
-    res.render('admin', { title: 'admin' });
+    res.render('admin', { title: 'admin', id: req.user.id });
 });
 
 router.get('/user', isAdmin, async function (req, res) {
