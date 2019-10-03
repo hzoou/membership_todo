2dd74c9a52d7b7f30667339bf91bd02cd098c5d8
hzoou <woojooo52@naver.com>
2019-10-03 20:33:45 +0900

    Modify path of api confirming duplicate id
    아이디 중복 검사는 회원가입에서 필요한 api 인데, 실수로 로그인 라우터에 추가한 것을 고침

--- web/routes/signin.js
+++ web/routes/signin.js
@@ -2,16 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const passport = require('../middlewares/passport');
-const USER = require('../models/user');
 
 router.get('/', function(req, res) {
     res.render('signin', { title: 'sign in' });
-});
-
-router.get('/:id', async function (req, res) {
-    const user = await USER.findUser(req.params.id);
-    if (user) return res.status(400).send({ status: 'FAIL', message: '이미 존재하는 아이디입니다.'});
-    res.status(200).send({ status: 'SUCCESS', message: '사용 가능한 아이디입니다.'});
 });
 
 router.post('/',  passport.authenticate('local', {
--- web/routes/signup.js
+++ web/routes/signup.js
@@ -6,6 +6,12 @@ router.get('/', function(req, res) {
     res.render('signup', { title: 'sign up' });
 });
 
+router.get('/:id', async function (req, res) {
+    const user = await USER.findUser(req.params.id);
+    if (user) return res.status(400).send({ status: 'FAIL', message: '이미 존재하는 아이디입니다.'});
+    res.status(200).send({ status: 'SUCCESS', message: '사용 가능한 아이디입니다.'});
+});
+
 router.post('/', async function (req, res) {
     await USER.insertUser(req.body.id, req.body.pw);
     res.redirect(307, '/signin')
