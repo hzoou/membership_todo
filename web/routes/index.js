e0ef4c7b7b320cbe4c99571b0e42ab17d0ed0047
hzoou <woojooo52@naver.com>
2019-10-03 23:31:15 +0900

    Modify index UI
    관리자인 경우, 헤더에 관리자 페이지 링크 추가

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { id: (req.user) ? req.user.id : '' });
+  res.render('index', { id: (req.user) ? req.user.id : '', admin: (req.user && req.user.admin) ? true : false });
 });
 
 module.exports = router;
