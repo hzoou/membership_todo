99b9363c335577aabe0e10c02fdd759428b97d10
hzoou <woojooo52@naver.com>
2019-10-03 19:47:02 +0900

    Modify index UI
    github 로고와 로그인한 경우, 사용자 아이디를 보여주는 헤더 추가

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { id: (req.user) ? req.user.id : '' });
 });
 
 module.exports = router;
