b87457cc07a74d7a52377a0da3b13577699c1e31
hzoou <woojooo52@naver.com>
2019-10-03 23:33:18 +0900

    Modify passport successRedirect path
    로그인 성공 후 메인 페이지로 이동

--- web/routes/signin.js
+++ web/routes/signin.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res) {
 });
 
 router.post('/',  passport.authenticate('local', {
-    successRedirect: '/board',
+    successRedirect: '/',
     failureRedirect: '/error?msg=아이디+또는+비밀번호를+확인해주세요.&url=signin',
     failureFlash: true })
 );
