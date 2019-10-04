c4b5cc6dbba7d297bb831cb400d1e0b18c9a2f2f
hzoou <woojooo52@naver.com>
2019-10-04 17:13:57 +0900

    Modify query to connect mysql
    이중으로 된 try-catch문 수정

--- web/schema/query.js
+++ web/schema/query.js
@@ -2,71 +2,59 @@ const pool = require('../database/connection').pool();
 
 module.exports = {
     getBoardIdxByUserId : async (id) => {
+        let connection;
         try {
-            const connection = await pool.getConnection(async conn => conn);
-            try {
-                const [results] = await connection.execute('SELECT BOARD.idx, BOARD.private FROM BOARD WHERE BOARD.USER_idx = (SELECT USER.idx FROM USER WHERE USER.id = ?)', [id]);
-                await connection.commit();
-                return results;
-            } catch (e) {
-                await connection.rollback();
-                return false;
-            } finally {
-                connection.release();
-            }
+            connection = await pool.getConnection(async conn => conn);
+            const [results] = await connection.execute('SELECT BOARD.idx, BOARD.private FROM BOARD WHERE BOARD.USER_idx = (SELECT USER.idx FROM USER WHERE USER.id = ?)', [id]);
+            await connection.commit();
+            return results;
         } catch (e) {
+            await connection.rollback();
             return false;
+        } finally {
+            connection.release();
         }
     },
 
     getAllListByBoard : async (board_idx) => {
+        let connection;
         try {
-            const connection = await pool.getConnection(async conn => conn);
-            try {
-                const [results] = await connection.execute('SELECT L.idx as LIST_idx, I.idx as ITEM_idx, L.title as LIST_title, I.title as ITEM_title FROM LIST L LEFT JOIN ITEM I ON I.LIST_idx = L.idx WHERE L.BOARD_idx = ?', [board_idx]);
-                await connection.commit();
-                return results;
-            } catch (e) {
-                await connection.rollback();
-                return false;
-            } finally {
-                connection.release();
-            }
+            connection = await pool.getConnection(async conn => conn);
+            const [results] = await connection.execute('SELECT L.idx as LIST_idx, I.idx as ITEM_idx, L.title as LIST_title, I.title as ITEM_title FROM LIST L LEFT JOIN ITEM I ON I.LIST_idx = L.idx WHERE L.BOARD_idx = ? ORDER BY I.idx DESC', [board_idx]);
+            await connection.commit();
+            return results;
         } catch (e) {
+            await connection.rollback();
             return false;
+        } finally {
+            connection.release();
         }
     },
 
     isAuthorizedUser : async (userIdx, boardIdx) => {
+        let connection;
         try {
-            const connection = await pool.getConnection(async conn => conn);
-            try {
-                const [results] = await connection.execute('SELECT * FROM PERMISSION WHERE USER_idx = ? AND BOARD_idx = ?', [userIdx, boardIdx]);
-                await connection.commit();
-                return results;
-            } catch (e) {
-                await connection.rollback();
-                return false;
-            } finally {
-                connection.release();
-            }
+            connection = await pool.getConnection(async conn => conn);
+            const [results] = await connection.execute('SELECT * FROM PERMISSION WHERE USER_idx = ? AND BOARD_idx = ?', [userIdx, boardIdx]);
+            await connection.commit();
+            return results;
         } catch (e) {
+            await connection.rollback();
             return false;
+        } finally {
+            connection.release();
         }
     },
 
     insertItem : async (title, list_idx) => {
-        const connection = await pool.getConnection(async conn => conn);
+        let connection;
         try {
-            try {
-                const [results] = await connection.execute('INSERT INTO ITEM (title, LIST_idx) VALUES (?, ?)', [title, list_idx]);
-                await connection.commit();
-                return results;
-            } catch (e) {
-                await connection.rollback();
-                return false;
-            }
+            connection = await pool.getConnection(async conn => conn);
+            const [results] = await connection.execute('INSERT INTO ITEM (title, LIST_idx) VALUES (?, ?)', [title, list_idx]);
+            await connection.commit();
+            return results;
         } catch (e) {
+            await connection.rollback();
             return false;
         } finally {
             connection.release();
@@ -74,164 +62,137 @@ module.exports = {
     },
 
     deleteItem : async (item_idx) => {
+        let connection;
         try {
-            const connection = await pool.getConnection(async conn => conn);
-            try {
-                const [results] = await connection.execute('DELETE FROM ITEM WHERE idx = ?', [item_idx]);
-                await connection.commit();
-                return results;
-            } catch (e) {
-                await connection.rollback();
-                return false;
-            } finally {
-                connection.release();
-            }
+            connection = await pool.getConnection(async conn => conn);
+            const [results] = await connection.execute('DELETE FROM ITEM WHERE idx = ?', [item_idx]);
+            await connection.commit();
+            return results;
         } catch (e) {
+            await connection.rollback();
             return false;
+        } finally {
+            connection.release();
         }
     },
 
     updateItem : async (title, content, item_idx) => {
+        let connection;
         try {
-            const connection = await pool.getConnection(async conn => conn);
-            try {
-                const [results] = await connection.execute('UPDATE ITEM SET title = ?, content = ? WHERE idx = ?', [title, content, item_idx]);
-                await connection.commit();
-                return results;
-            } catch (e) {
-                await connection.rollback();
-                return false;
-            } finally {
-                connection.release();
-            }
+            connection = await pool.getConnection(async conn => conn);
+            const [results] = await connection.execute('UPDATE ITEM SET title = ?, content = ? WHERE idx = ?', [title, content, item_idx]);
+            await connection.commit();
+            return results;
         } catch (e) {
+            await connection.rollback();
             return false;
+        } finally {
+            connection.release();
         }
     },
 
     findUser : async (id) => {
+        let connection;
         try {
-            const connection = await pool.getConnection(async conn => conn);
-            try {
-                const [results] = await connection.execute('SELECT * FROM USER WHERE id = ?', [id]);
-                await connection.commit();
-                return results;
-            } catch (e) {
-                await connection.rollback();
-                return false;
-            } finally {
-                connection.release();
-            }
+            connection = await pool.getConnection(async conn => conn);
+            const [results] = await connection.execute('SELECT * FROM USER WHERE id = ?', [id]);
+            await connection.commit();
+            return results;
         } catch (e) {
+            await connection.rollback();
             return false;
+        } finally {
+            connection.release();
         }
     },
 
     insertUser : async (id, pw) => {
+        let connection;
         try {
-            const connection = await pool.getConnection(async conn => conn);
-            try {
-                const [results] = await connection.execute('INSERT INTO USER (id, pw) VALUES (?, ?)', [id, pw]);
-                await connection.commit();
-                return results;
-            } catch (e) {
-                await connection.rollback();
-                return false;
-            } finally {
-                connection.release();
-            }
+            connection = await pool.getConnection(async conn => conn);
+            const [results] = await connection.execute('INSERT INTO USER (id, pw) VALUES (?, ?)', [id, pw]);
+            await connection.commit();
+            return results;
         } catch (e) {
+            await connection.rollback();
             return false;
+        } finally {
+            connection.release();
         }
     },
 
     makeBoard : async (user_idx) => {
+        let connection;
         try {
-            const connection = await pool.getConnection(async conn => conn);
-            try {
-                const [results] = await connection.execute('INSERT INTO BOARD (USER_idx) VALUES (?)', [user_idx]);
-                await connection.commit();
-                return results;
-            } catch (e) {
-                await connection.rollback();
-                return false;
-            } finally {
-                connection.release();
-            }
+            connection = await pool.getConnection(async conn => conn);
+            const [results] = await connection.execute('INSERT INTO BOARD (USER_idx) VALUES (?)', [user_idx]);
+            await connection.commit();
+            return results;
         } catch (e) {
+            await connection.rollback();
             return false;
+        } finally {
+            connection.release();
         }
     },
 
     makeList : async (title, board_idx) => {
+        let connection;
         try {
-            const connection = await pool.getConnection(async conn => conn);
-            try {
-                const [results] = await connection.execute('INSERT INTO LIST (title, BOARD_idx) VALUES (?, ?)', [title, board_idx]);
-                await connection.commit();
-                return results;
-            } catch (e) {
-                await connection.rollback();
-                return false;
-            } finally {
-                connection.release();
-            }
+            connection = await pool.getConnection(async conn => conn);
+            const [results] = await connection.execute('INSERT INTO LIST (title, BOARD_idx) VALUES (?, ?)', [title, board_idx]);
+            await connection.commit();
+            return results;
         } catch (e) {
+            await connection.rollback();
             return false;
+        } finally {
+            connection.release();
         }
     },
     
     getAllUser : async () => {
+        let connection;
         try {
-            const connection = await pool.getConnection(async conn => conn);
-            try {
-                const [results] = await connection.execute('SELECT idx, id, admin FROM USER');
-                await connection.commit();
-                return results;
-            } catch (e) {
-                await connection.rollback();
-                return false;
-            } finally {
-                connection.release();
-            }
+            connection = await pool.getConnection(async conn => conn);
+            const [results] = await connection.execute('SELECT idx, id, admin FROM USER');
+            await connection.commit();
+            return results;
         } catch (e) {
+            await connection.rollback();
             return false;
+        } finally {
+            connection.release();
         }
     },
 
     updateUser : async (idx, admin) => {
+        let connection;
         try {
-            const connection = await pool.getConnection(async conn => conn);
-            try {
-                const [results] = await connection.execute('UPDATE USER SET admin = ? WHERE idx = ?', [admin, idx]);
-                await connection.commit();
-                return results;
-            } catch (e) {
-                await connection.rollback();
-                return false;
-            } finally {
-                connection.release();
-            }
+            connection = await pool.getConnection(async conn => conn);
+            const [results] = await connection.execute('UPDATE USER SET admin = ? WHERE idx = ?', [admin, idx]);
+            await connection.commit();
+            return results;
         } catch (e) {
+            await connection.rollback();
             return false;
+        } finally {
+            connection.release();
         }
     },
 
     deleteUser : async (idx) => {
+        let connection;
         try {
-            const connection = await pool.getConnection(async conn => conn);
-            try {
-                const [results] = await connection.execute('DELETE FROM USER WHERE idx = ?', [idx]);
-                await connection.commit();
-                return results;
-            } catch (e) {
-                await connection.rollback();
-                return false;
-            } finally {
-                connection.release();
-            }
+            connection = await pool.getConnection(async conn => conn);
+            const [results] = await connection.execute('DELETE FROM USER WHERE idx = ?', [idx]);
+            await connection.commit();
+            return results;
         } catch (e) {
+            await connection.rollback();
             return false;
+        } finally {
+            connection.release();
         }
     }
 };
