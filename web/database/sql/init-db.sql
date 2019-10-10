7b4e5ec32efd5011f495c5adde1ec7fc188e1fdd
hzoou <woojooo52@naver.com>
2019-10-10 23:39:50 +0900

    Modify db init sql statement
    item 테이블에 pos column 추가
    초기 데이터 삽입 시 pos값 추가

--- web/database/sql/init-db.sql
+++ web/database/sql/init-db.sql
@@ -69,8 +69,9 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS ITEM;
 CREATE TABLE IF NOT EXISTS `todo`.`ITEM` (
   `idx` INT NOT NULL AUTO_INCREMENT,
-  `title` TEXT,
+  `title` TEXT NOT NULL,
   `LIST_idx` INT NOT NULL,
+  `pos` DECIMAL(14,4),
   PRIMARY KEY (`idx`),
   INDEX `fk_ITEM_LIST1_idx` (`LIST_idx` ASC) VISIBLE,
   CONSTRAINT `fk_ITEM_LIST1`
@@ -116,8 +117,8 @@ CREATE TABLE IF NOT EXISTS `todo`.`LOG` (
   `user_id` VARCHAR(45) NOT NULL,
   `BOARD_idx` INT NOT NULL,
   `item_title` TEXT NOT NULL,
-  `source` TEXT NOT NULL,
-  `target` TEXT NOT NULL,
+  `source` TEXT,
+  `target` TEXT,
   `action` TINYINT NOT NULL,
   `time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`idx`),
@@ -141,37 +142,33 @@ INSERT INTO BOARD (USER_idx) VALUES (1);
 INSERT INTO LIST (title, BOARD_idx) VALUES ('To Do', 1);
 INSERT INTO LIST (title, BOARD_idx) VALUES ('In Progress', 1);
 INSERT INTO LIST (title, BOARD_idx) VALUES ('Done', 1);
-INSERT INTO ITEM (title, LIST_idx) VALUES ('프론트', 1);
-INSERT INTO ITEM (title, LIST_idx) VALUES ('api 설계', 2);
-INSERT INTO ITEM (title, LIST_idx) VALUES ('테이블 설계', 3);
-INSERT INTO ITEM (title, LIST_idx) VALUES ('백엔드', 2);
+INSERT INTO ITEM (title, LIST_idx, pos) VALUES ('프론트', 1, 5000000);
+INSERT INTO ITEM (title, LIST_idx, pos) VALUES ('api 설계', 2, 5000000);
+INSERT INTO ITEM (title, LIST_idx, pos) VALUES ('테이블 설계', 3, 5000000);
 INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('admin', 1, '프론트', 'To Do', 0);
 INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('admin', 1, 'api 설계', 'In Progress', 0);
 INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('admin', 1, '테이블 설계', 'Done', 0);
-INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('admin', 1, '백엔드', 'In Progress', 0);
 
 INSERT INTO USER (id, pw, admin) VALUES ('hzoou', '1ARVn2Auq2/WAqx2gNrL+q3RNjAzXpUfCXrzkA6d4Xa22yhRLy4AC50E+6UTPoscbo31nbOoq51gvkuXzJ6B2w==', 0);
 INSERT INTO BOARD (USER_idx) VALUES (2);
 INSERT INTO LIST (title, BOARD_idx) VALUES ('To Do', 2);
 INSERT INTO LIST (title, BOARD_idx) VALUES ('In Progress', 2);
 INSERT INTO LIST (title, BOARD_idx) VALUES ('Done', 2);
-INSERT INTO ITEM (title, LIST_idx) VALUES ('공부하기', 4);
-INSERT INTO ITEM (title, LIST_idx) VALUES ('밥먹기', 5);
-INSERT INTO ITEM (title, LIST_idx) VALUES ('놀기', 6);
-INSERT INTO ITEM (title, LIST_idx) VALUES ('잠자기', 5);
+INSERT INTO ITEM (title, LIST_idx, pos) VALUES ('공부하기', 4, 5000000);
+INSERT INTO ITEM (title, LIST_idx, pos) VALUES ('밥먹기', 5, 5000000);
+INSERT INTO ITEM (title, LIST_idx, pos) VALUES ('놀기', 6, 5000000);
 INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('hzoou', 2, '공부하기', 'To Do', 0);
 INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('hzoou', 2, '밥먹기', 'In Progress', 0);
 INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('hzoou', 2, '놀기', 'Done', 0);
-INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('hzoou', 2, '잠자기', 'In Progress', 0);
 
 INSERT INTO USER (id, pw, admin) VALUES ('lallaheee', '1ARVn2Auq2/WAqx2gNrL+q3RNjAzXpUfCXrzkA6d4Xa22yhRLy4AC50E+6UTPoscbo31nbOoq51gvkuXzJ6B2w==', 0);
 INSERT INTO BOARD (USER_idx) VALUES (3);
 INSERT INTO LIST (title, BOARD_idx) VALUES ('To Do', 3);
 INSERT INTO LIST (title, BOARD_idx) VALUES ('In Progress', 3);
 INSERT INTO LIST (title, BOARD_idx) VALUES ('Done', 3);
-INSERT INTO ITEM (title, LIST_idx) VALUES ('수영하기', 7);
-INSERT INTO ITEM (title, LIST_idx) VALUES ('orm 만들기', 8);
-INSERT INTO ITEM (title, LIST_idx) VALUES ('마라탕 먹기', 9);
+INSERT INTO ITEM (title, LIST_idx, pos) VALUES ('수영하기', 7, 5000000);
+INSERT INTO ITEM (title, LIST_idx, pos) VALUES ('orm 만들기', 8, 5000000);
+INSERT INTO ITEM (title, LIST_idx, pos) VALUES ('마라탕 먹기', 9, 5000000);
 INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('lallaheee', 3, '수영하기', 'To Do', 0);
 INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('lallaheee', 3, 'orm 만들기', 'In Progress', 0);
 INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('lallaheee', 3, '마라탕 먹기', 'Done', 0);
@@ -181,13 +178,11 @@ INSERT INTO BOARD (USER_idx) VALUES (4);
 INSERT INTO LIST (title, BOARD_idx) VALUES ('To Do', 4);
 INSERT INTO LIST (title, BOARD_idx) VALUES ('In Progress', 4);
 INSERT INTO LIST (title, BOARD_idx) VALUES ('Done', 4);
-INSERT INTO ITEM (title, LIST_idx) VALUES ('공부하기', 10);
-INSERT INTO ITEM (title, LIST_idx) VALUES ('팟타이 만들기', 11);
-INSERT INTO ITEM (title, LIST_idx) VALUES ('수영하기', 12);
-INSERT INTO ITEM (title, LIST_idx) VALUES ('조커 보기', 12);
-INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('samrho', 4, '수영하기', 'To Do', 0);
+INSERT INTO ITEM (title, LIST_idx, pos) VALUES ('공부하기', 10, 5000000);
+INSERT INTO ITEM (title, LIST_idx, pos) VALUES ('팟타이 만들기', 11, 5000000);
+INSERT INTO ITEM (title, LIST_idx, pos) VALUES ('조커 보기', 12, 5000000);
+INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('samrho', 4, '공부하기', 'To Do', 0);
 INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('samrho', 4, '팟타이 만들기', 'In Progress', 0);
-INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('samrho', 4, '수영하기', 'Done', 0);
 INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('samrho', 4, '조커 보기', 'Done', 0);
 
 
@@ -196,9 +191,9 @@ INSERT INTO BOARD (USER_idx) VALUES (5);
 INSERT INTO LIST (title, BOARD_idx) VALUES ('To Do', 5);
 INSERT INTO LIST (title, BOARD_idx) VALUES ('In Progress', 5);
 INSERT INTO LIST (title, BOARD_idx) VALUES ('Done', 5);
-INSERT INTO ITEM (title, LIST_idx) VALUES ('축구 보기', 13);
-INSERT INTO ITEM (title, LIST_idx) VALUES ('공부하기', 14);
-INSERT INTO ITEM (title, LIST_idx) VALUES ('풋살하기', 15);
+INSERT INTO ITEM (title, LIST_idx, pos) VALUES ('축구 보기', 13, 5000000);
+INSERT INTO ITEM (title, LIST_idx, pos) VALUES ('밥먹기', 14, 5000000);
+INSERT INTO ITEM (title, LIST_idx, pos) VALUES ('풋살하기', 15, 5000000);
 INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('eastgerm', 5, '축구 보기', 'To Do', 0);
-INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('eastgerm', 5, '공부하기', 'In Progress', 0);
+INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('eastgerm', 5, '밥먹기', 'In Progress', 0);
 INSERT INTO LOG (user_id, BOARD_idx, item_title, target, action) VALUES ('eastgerm', 5, '풋살하기', 'Done', 0);
