37c48f33e60a8e5f4cf9b86ce6052b7fd8923d4b
hzoou <woojooo52@naver.com>
2019-10-11 02:15:16 +0900

    Modify board UI
    log를 보여주는 div element에 overflow hidden 적용

--- web/public/javascripts/utils.js
+++ web/public/javascripts/utils.js
@@ -24,6 +24,6 @@ const $$ = (selectors) => {
 const getTime = (time) => {
     const date = new Date(time);
     return `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-}
+};
 
 export {$, $$, getTime, fetchAPI};
